ebb516c7aa5a1ef3cd2acf423cbabe167865d81c
zachgur <zfriedel@zachguru.com>
2024-01-31 16:50:41 -0500

    add search district functionality. Update district map component. Add container component for searching and map display

--- src/components/DistrictMap.tsx
+++ src/components/DistrictMap.tsx
@@ -1,39 +1,61 @@
-import React from 'react';
-import { MapContainer, TileLayer, Marker } from 'react-leaflet';
+import React, { useEffect, useRef, useState } from 'react';
+import { MapContainer, TileLayer, Marker, useMapEvents, useMap, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
-import L, { Icon } from 'leaflet';
-import locationIcon from 'public/location.png'
+import L, { Icon, MapOptions } from 'leaflet';
+import locationIcon from '/placeholder.png';
+import { NCESDistrictFeatureAttributes } from '@utils/nces';
+
+interface DistrictMapProps {
+  center: [number, number];
+  zoom: number;
+  markers: NCESDistrictFeatureAttributes[];
+}
+
+interface MarkerData {
+  data: NCESDistrictFeatureAttributes[];
+}
 
 const customIcon = new Icon({
   iconUrl: locationIcon,
   iconSize: [38, 38],
 });
 
-const markers = [
-  {
-    geocode: L.latLng(48.86, 2.3522),
-    popUp: 'Pop 1',
-  },
-  {
-    geocode: L.latLng(48.85, 2.3522),
-    popUp: 'Pop 2',
-  },
-  {
-    geocode: L.latLng(48.855, 2.34),
-    popUp: 'Pop 3',
-  },
-];
+const Markers: React.FC<MarkerData> = ({ data }) => {
+  const map = useMap();
+  return data.length > 0 ? (
+    <>
+      {data.map((marker, index) => (
+        <Marker
+          key={index}
+          position={[marker.LAT1516, marker.LON1516]}
+          icon={customIcon}
+          eventHandlers={{
+            click: () => {
+              map.setView([marker.LAT1516, marker.LON1516], 14);
+            },
+          }}
+        >
+          <Popup>
+            <div>
+              <p><strong>Name: </strong> {marker.NAME}</p>
+              <p><strong>Location: </strong> {marker.LCITY}, {marker.LSTATE}, {marker.LZIP}</p>
+              <p><strong>County: </strong>{marker.NMCNTY15}</p>
+            </div>
+          </Popup>
+        </Marker>
+      ))}
+    </>
+  ) : null;
+};
 
-const DistrictMap: React.FC = ({ children }) => {
+const DistrictMap: React.FC<DistrictMapProps> = ({ center, zoom, markers }) => {
   return (
-    <MapContainer center={[48.8566, 2.3522]} zoom={13}>
+    <MapContainer center={center} zoom={zoom}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      {markers.map((marker) => (
-        <Marker key={marker.geocode} position={marker.geocode} icon={customIcon}></Marker>
-      ))}
+      <Markers data={markers} />
     </MapContainer>
   );
 };
---
+++ src/components/FreshMap.tsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from 'react';
+import {
+  Button,
+  Input,
+  Spinner,
+  InputGroup, // Some Chakra components that might be usefull
+  Container,
+  InputRightElement,
+  Text,
+} from '@chakra-ui/react';
+import {
+  searchSchoolDistricts,
+  searchSchools,
+  NCESDistrictFeatureAttributes,
+  NCESSchoolFeatureAttributes,
+} from '../utils/nces';
+import DistrictMap from './DistrictMap';
+import L from 'leaflet';
+
+const FreshMap: React.FC = () => {
+  const [searching, setSearching] = React.useState(false);
+  const [district, setDistrict] = useState('');
+  const [dataMarkers, setDataMarkers] = useState<NCESDistrictFeatureAttributes[]>([]);
+  const [center, setCenter] = useState<[number, number]>([39.8283, -98.5795]);
+  const [zoom, setZoom] = useState(4);
+
+  const handleDistrictChange = (event: any) => {
+    setDistrict(event.target.value);
+  };
+
+  const handleSearch = async () => {
+    if(district=='')  return;
+    setSearching(true);
+    const data = await searchSchoolDistricts(district);
+    setDataMarkers(data);
+    setSearching(false);
+  };
+
+  return (
+    <Container>
+      <InputGroup>
+        <Input size="md" placeholder="Search for district" value={district} onChange={handleDistrictChange} />
+        <InputRightElement width="4.5rem">
+          <Button h="1.75rem" size="sm" onClick={handleSearch}>
+            Search
+          </Button>
+        </InputRightElement>
+      </InputGroup>
+      {searching ? <Spinner width="75vh" /> : <DistrictMap center={center} zoom={zoom} markers={dataMarkers} />}
+    </Container>
+  );
+};
+
+export default FreshMap;
