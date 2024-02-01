a5417c357182a1f69504237a5483ad584d93d722
zachgur <zfriedel@zachguru.com>
2024-02-01 17:17:10 -0500

    displaying only neccesery school data

--- src/components/design/SchoolTable.tsx
+++ src/components/design/SchoolTable.tsx
@@ -9,62 +9,56 @@ export interface SchoolTableProps {
 export const SchoolTable: React.FC<SchoolTableProps> = ({ data }) => {
   return (
     <TableContainer margin="10px">
-      <Table variant="striped" colorScheme="green">
+      <Table size="sm" variant="striped" colorScheme="green">
         <TableCaption>Schools and school districts</TableCaption>
         <Thead>
           <Tr>
-            <Th>CBSA 15</Th>
-            <Th>CBSA TYPE 15</Th>
-            <Th>CD15</Th>
-            <Th>CNTY15</Th>
-            <Th>CSA15</Th>
-            <Th>LAT1516</Th>
-            <Th>LCITY</Th>
             <Th>LEAID</Th>
-            <Th>LON1516</Th>
-            <Th>LSTREET</Th>
+            <Th>NAME</Th>
+            <Th>LSTATE</Th>
+            <Th>CNTY15</Th>
+            <Th>NMCNTY15</Th>
+            <Th>CBSA15</Th>
+            <Th>NMCBSA15</Th>
+            <Th>CD15</Th>
+            <Th>SLDL15</Th>
+            <Th>SLDU15</Th>
             <Th>LZIP</Th>
             <Th>LZIP4</Th>
-            <Th>NAME</Th>
-            <Th>NECTA15</Th>
           </Tr>
         </Thead>
         <Tbody>
           {data.map((school, index) => (
             <Tr>
-              <Td>{school.CBSA15}</Td>
-              <Td>{school.CBSATYPE15}</Td>
-              <Td>{school.CD15}</Td>
-              <Td>{school.CNTY15}</Td>
-              <Td>{school.CSA15}</Td>
-              <Td>{school.LAT1516}</Td>
-              <Td>{school.LCITY}</Td>
               <Td>{school.LEAID}</Td>
-              <Td>{school.LON1516}</Td>
-              <Td>{school.LSTREE}</Td>
+              <Td>{school.NAME}</Td>
+              <Td>{school.LSTATE}</Td>
+              <Td>{school.CNTY15}</Td>
+              <Td>{school.NMCNTY15}</Td>
+              <Td>{school.CBSA15}</Td>
+              <Td>{school.NMCBSA15}</Td>
+              <Td>{school.CD15}</Td>
+              <Td>{school.SLDL15}</Td>
+              <Td>{school.SLDU15}</Td>
               <Td>{school.LZIP}</Td>
               <Td>{school.LZIP4}</Td>
-              <Td>{school.NAME}</Td>
-              <Td>{school.NECTA15}</Td>
             </Tr>
           ))}
         </Tbody>
         <Tfoot>
           <Tr>
-            <Th>CBSA 15</Th>
-            <Th>CBSA TYPE 15</Th>
-            <Th>CD15</Th>
-            <Th>CNTY15</Th>
-            <Th>CSA15</Th>
-            <Th>LAT1516</Th>
-            <Th>LCITY</Th>
             <Th>LEAID</Th>
-            <Th>LON1516</Th>
-            <Th>LSTREET</Th>
+            <Th>NAME</Th>
+            <Th>LSTATE</Th>
+            <Th>CNTY15</Th>
+            <Th>NMCNTY15</Th>
+            <Th>CBSA15</Th>
+            <Th>NMCBSA15</Th>
+            <Th>CD15</Th>
+            <Th>SLDL15</Th>
+            <Th>SLDU15</Th>
             <Th>LZIP</Th>
             <Th>LZIP4</Th>
-            <Th>NAME</Th>
-            <Th>NECTA15</Th>
           </Tr>
         </Tfoot>
       </Table>
