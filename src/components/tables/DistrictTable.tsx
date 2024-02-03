e458eb3fac323de5284a5b1c86ff3a1e64b76c55
zachgur <zfriedel@zachguru.com>
2024-02-03 11:40:06 -0500

    add pagination to table views

--- src/components/tables/DistrictTable.tsx
+++ src/components/tables/DistrictTable.tsx
@@ -1,77 +1,39 @@
-import { Table, Thead, Tbody, Tfoot, Tr, Th, Td, TableCaption, TableContainer } from '@chakra-ui/react';
+import {Table,Thead,Tr,Th,TableCaption,TableContainer} from '@chakra-ui/react';
+import { PaginatedData } from '@components/design/PaginationData';
 import { NCESDistrictFeatureAttributes } from '@utils/nces';
 import React from 'react';
-
+  
 export interface DistrictTableProps {
-  data: NCESDistrictFeatureAttributes[];
-}
-
+    data: NCESDistrictFeatureAttributes[];
+  }
+  
 export const DistrictTable: React.FC<DistrictTableProps> = ({ data }) => {
-  return (
-    <>
-      <TableContainer margin="10px">
-        <Table size="sm" variant="striped" colorScheme="green">
-          <TableCaption>Schools and school districts</TableCaption>
-          <Thead>
-            <Tr>
-              <Th>LEAID</Th>
-              <Th>NAME</Th>
-              <Th>LSTATE</Th>
-              <Th>CNTY15</Th>
-              <Th>NMCNTY15</Th>
-              <Th>CBSA15</Th>
-              <Th>NMCBSA15</Th>
-              <Th>CD15</Th>
-              <Th>SLDL15</Th>
-              <Th>SLDU15</Th>
-              <Th>LZIP</Th>
-              <Th>LZIP4</Th>
-            </Tr>
-          </Thead>
-          <Tbody>
-            {data.length > 0 ? (
-              data.map((school, index) => (
-                <Tr>
-                  <Td>{school.LEAID}</Td>
-                  <Td>{school.NAME}</Td>
-                  <Td>{school.LSTATE}</Td>
-                  <Td>{school.CNTY15}</Td>
-                  <Td>{school.NMCNTY15}</Td>
-                  <Td>{school.CBSA15}</Td>
-                  <Td>{school.NMCBSA15}</Td>
-                  <Td>{school.CD15}</Td>
-                  <Td>{school.SLDL15}</Td>
-                  <Td>{school.SLDU15}</Td>
-                  <Td>{school.LZIP}</Td>
-                  <Td>{school.LZIP4}</Td>
-                </Tr>
-              ))
-            ) : (
+  
+    return (
+      <>
+        <TableContainer margin="10px">
+          <Table size="sm" variant="striped" colorScheme="green">
+            <TableCaption>Schools and school districts</TableCaption>
+            <Thead>
               <Tr>
-                <Td colSpan={12} style={{ textAlign: 'center' }}>
-                  No data
-                </Td>
+                <Th>LEAID</Th>
+                <Th>NAME</Th>
+                <Th>LSTATE</Th>
+                <Th>CNTY15</Th>
+                <Th>NMCNTY15</Th>
+                <Th>CBSA15</Th>
+                <Th>NMCBSA15</Th>
+                <Th>CD15</Th>
+                <Th>SLDL15</Th>
+                <Th>SLDU15</Th>
+                <Th>LZIP</Th>
+                <Th>LZIP4</Th>
               </Tr>
-            )}
-          </Tbody>
-          <Tfoot>
-            <Tr>
-              <Th>LEAID</Th>
-              <Th>NAME</Th>
-              <Th>LSTATE</Th>
-              <Th>CNTY15</Th>
-              <Th>NMCNTY15</Th>
-              <Th>CBSA15</Th>
-              <Th>NMCBSA15</Th>
-              <Th>CD15</Th>
-              <Th>SLDL15</Th>
-              <Th>SLDU15</Th>
-              <Th>LZIP</Th>
-              <Th>LZIP4</Th>
-            </Tr>
-          </Tfoot>
-        </Table>
-      </TableContainer>
-    </>
-  );
-};
+            </Thead>
+            <PaginatedData data={data} type="district" />
+          </Table>
+        </TableContainer>
+      </>
+    );
+  };
+  
--- src/components/tables/SchoolsTable.tsx
+++ src/components/tables/SchoolsTable.tsx
@@ -1,6 +1,7 @@
-import { Table, Thead, Tbody, Tfoot, Tr, Th, Td, TableCaption, TableContainer } from '@chakra-ui/react';
+import { Table, Thead, Tr, Th, TableCaption, TableContainer } from '@chakra-ui/react';
+import { PaginatedData } from '@components/design/PaginationData';
 import { NCESSchoolFeatureAttributes } from '@utils/nces';
-import React, { useState } from 'react';
+import React from 'react';
 
 export interface SchoolTableProps {
   data: NCESSchoolFeatureAttributes[];
@@ -27,46 +28,7 @@ export const SchoolsTable: React.FC<SchoolTableProps> = ({ data }) => {
               <Th>LOCALE</Th>
             </Tr>
           </Thead>
-          <Tbody>
-            {data.length > 0 ? (
-              data.map((school, index) => (
-                <Tr>
-                  <Td>{school.NAME}</Td>
-                  <Td>{school.LEAID}</Td>
-                  <Td>{school.STREET}</Td>
-                  <Td>{school.CITY}</Td>
-                  <Td>{school.STATE}</Td>
-                  <Td>{school.ZIP}</Td>
-                  <Td>{school.NCESSCH}</Td>
-                  <Td>{school.OPSTFIPS}</Td>
-                  <Td>{school.STFIP}</Td>
-                  <Td>{school.NMCNTY}</Td>
-                  <Td>{school.LOCALE}</Td>
-                </Tr>
-              ))
-            ) : (
-              <Tr>
-                <Td colSpan={12} style={{ textAlign: 'center' }}>
-                  No data
-                </Td>
-              </Tr>
-            )}
-          </Tbody>
-          <Tfoot>
-            <Tr>
-              <Th>NAME</Th>
-              <Th>LEAID</Th>
-              <Th>STREET</Th>
-              <Th>CITY</Th>
-              <Th>STATE</Th>
-              <Th>ZIP</Th>
-              <Th>NCESSCH</Th>
-              <Th>OPSTFIPS</Th>
-              <Th>STFIP</Th>
-              <Th>NMCNTY</Th>
-              <Th>LOCALE</Th>
-            </Tr>
-          </Tfoot>
+          <PaginatedData data={data} type='school'/>
         </Table>
       </TableContainer>
     </>
